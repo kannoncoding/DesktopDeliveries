d9a30f760af4cc77d7e8dce3b994145813a661d5
kannoncoding <codingadaptations@gmail.com>
2025-06-14 20:56:37 -0600

    initial setup first class

---
+++ Entregas.Entidades/TipoArticulo.cs
@@ -0,0 +1,40 @@
+﻿// Universidad Estatal a Distancia (UNED)
+// II Cuatrimestre 2025
+// Programación Avanzada con C# - Proyecto 1
+// Jorge Luis Arias Melendez
+// Descripción: Representa una categoría/tipo de artículo para ENTREGAS S.A. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entregas.Entidades
+{
+
+    // Representa un tipo o categoría de artículo.
+    public class TipoArticulo
+    {
+        // Identificador único del tipo de artículo.
+        public int Id { get; set; }
+
+        // Nombre del tipo de artículo (requerido).
+        public required string Nombre { get; set; }
+
+        // Descripción breve del tipo de artículo (requerido).
+        public required string Descripcion { get; set; }
+
+        // Constructor vacío
+        public TipoArticulo() { }
+
+        // Constructor
+        public TipoArticulo(int id, string nombre, string descripcion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+    }
+
+}
