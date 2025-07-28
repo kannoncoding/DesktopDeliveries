012c6ef863cca52fd19f2d9a1f9b462e32f87138
kannoncoding <codingadaptations@gmail.com>
2025-07-27 21:48:41 -0600

    Connection class implemented, app.config setup completed

---
+++ Entregas.Datos/ConexionBD.cs
@@ -0,0 +1,30 @@
+﻿// Universidad Estatal a Distancia (UNED)
+// II Cuatrimestre 2025
+// Programación Avanzada con C# - Proyecto 2
+// Jorge Luis Arias Melendez
+// Descripción del archivo: Clase base para gestionar la conexión a SQL Server usando ADO.NET en ENTREGAS S.A.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Entregas.Datos
+{
+    public static class ConexionBD
+    {
+        // Obtiene la cadena de conexión desde App.config
+        private static readonly string cadenaConexion =
+            System.Configuration.ConfigurationManager.ConnectionStrings["EntregasBD"].ConnectionString;
+
+        // Método para obtener una conexión abierta
+        public static SqlConnection ObtenerConexion()
+        {
+            var conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
